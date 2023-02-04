2cbf70de443c4049a703d1ac3e597a43fbc6051e
sid <sidharth@divami.com>
2023-02-04 10:12:50 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CrudService } from './service/crud.service';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, DashboardComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [CrudService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
